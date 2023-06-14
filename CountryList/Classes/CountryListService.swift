f3050d2d0fd51cdb7b85d053b69e7131023b304b
Gabriel Moreira <g.alves.m.2008@gmail.com>
2023-06-14 13:40:15 -0300

    Added image download

--- CountryList/Classes/CountryListService.swift
+++ CountryList/Classes/CountryListService.swift
@@ -7,11 +7,11 @@ protocol CountryListServicing {
 final class CountryListService: CountryListServicing {
     func fetchCountries(completion: CountryListServiceCompletion) {
         completion([
-            Country(name: "England", capital: "London", flagImage: "eng", isKnown: true),
-            Country(name: "Brazil", capital: "Brasilia", flagImage: "bra", isKnown: true),
-            Country(name: "France", capital: "Paris", flagImage: "fra", isKnown: false),
-            Country(name: "Japan", capital: "Tokyo", flagImage: "jpn", isKnown: false),
-            Country(name: "Germany", capital: "Berlin", flagImage: "ger", isKnown: true)
+            Country(name: "England", capital: "London", flagImage: "https://cdn.britannica.com/44/344-004-494CC2E8/Flag-England.jpg", isKnown: true),
+            Country(name: "Brazil", capital: "Brasilia", flagImage: "https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/640px-Flag_of_Brazil.svg.png", isKnown: true),
+            Country(name: "France", capital: "Paris", flagImage: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/1200px-Flag_of_France.svg.png", isKnown: false),
+            Country(name: "Japan", capital: "Tokyo", flagImage: "https://e0.pxfuel.com/wallpapers/911/753/desktop-wallpaper-japan-flag-japanese-flag-flag-of-japan-japan.jpg", isKnown: false),
+            Country(name: "Germany", capital: "Berlin", flagImage: "https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/255px-Flag_of_Germany.svg.png", isKnown: true)
         ])
     }
 }
